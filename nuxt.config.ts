1b400e41431528d3175c12ad985e33af0a7924c6
promosixniner <promo69@protonmail.com>
2022-05-04 17:36:28 +0000

    gitpod working?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,42 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    buildModules: [
+        '@unocss/nuxt',
+    ],
+    css: [
+        '@/assets/css/main.css',
+    ],
+    vite: {
+        server: {
+            hmr: {
+                protocol: 'wss',
+                timeout: 10000,
+                port: 3001,
+            },
+        },
 
+        logLevel: 'info',
+    },
+    unocss: {
+        preflight: true,
+        icons: true,
+        attributify: true,
+        webFonts: {
+            fonts: {
+                standard: [
+                    {
+                        name: 'Poppins',
+                        weights: [
+                            '100',
+                            '200',
+                            '300',
+                            '400',
+                        ],
+                        italic: false,
+                    },
+                ],
+            },
+        },
+    },
 })
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+<div>
+    <h1 class="text-red-400">Hello!</h1>
+    <div>12</div>
+    <div></div>
+
+    <NuxtLink to='/test'>go to</NuxtLink>
+</div>
+</template>
