3e133c77bc5e539a81fa18f07eb169fd359f2c31
promosixniner <promo69@protonmail.com>
2022-05-04 18:00:56 +0000

    nvmrc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    buildModules: [
-        '@unocss/nuxt',
-    ],
     css: [
         '@/assets/css/main.css',
     ],
@@ -12,32 +9,10 @@ export default defineNuxtConfig({
         server: {
             hmr: {
                 protocol: 'wss',
-                timeout: 10000,
-                port: 3001,
+                timeout: 1,
             },
         },
 
         logLevel: 'info',
-    },
-    unocss: {
-        preflight: true,
-        icons: true,
-        attributify: true,
-        webFonts: {
-            fonts: {
-                standard: [
-                    {
-                        name: 'Poppins',
-                        weights: [
-                            '100',
-                            '200',
-                            '300',
-                            '400',
-                        ],
-                        italic: false,
-                    },
-                ],
-            },
-        },
     },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <h1 class="text-red-400">Hello!</h1>
-    <div>12</div>
+    <div>12345678</div>
     <div></div>
 
     <NuxtLink to='/test'>go to</NuxtLink>
